d38be5f1dc14f124ba50d54cd35893b8af19642a
Roí Rosales <rosalesroy11@gmail.com>
2024-08-09 20:23:16 -0600

    On main: !!GitHub_Desktop<main>

--- Transacciones/Program.cs
+++ Transacciones/Program.cs
@@ -12,6 +12,7 @@ namespace Transacciones
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+           
         }
     }
 }
